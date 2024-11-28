61a94982c4cef1de496a86de2cb66f94bf7d3249
Isaac <bennisaac24@gmail.com>
2024-11-28 13:35:48 +0300

    assignments done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 2
+const num2 = 31
+
+const multiply = (num1 * num2)
+
+
+
+const random = Math.ceil(Math.random());
+
+const num3 = 19;
+const num4 = 5;
+
+const mod = num3 % num4
+
+const max = Math.max(2, 4, 8, 10, 14, 18, 20)
 
 // Write your code here
